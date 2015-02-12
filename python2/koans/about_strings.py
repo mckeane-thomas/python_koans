45c3ee94e70ef2da310f7a8e41d969d71d6ef102
mckeane-thomas <mckeane.thomas@gmail.com>
2015-02-12 11:13:16 -0500

    Update about_strings.py

--- python2/koans/about_strings.py
+++ python2/koans/about_strings.py
@@ -83,17 +83,17 @@ world!
         hi = "Hello, "
         there = "world"
         hi += there
-        self.assertEqual(__, hi)
+        self.assertEqual("Hello, world", hi)
 
     def test_plus_equals_also_leaves_original_string_unmodified(self):
         original = "Hello, "
         hi = original
         there = "world"
         hi += there
-        self.assertEqual(__, original)
+        self.assertEqual("Hello, ", original)
 
     def test_most_strings_interpret_escape_characters(self):
         string = "\n"
         self.assertEqual('\n', string)
         self.assertEqual("""\n""", string)
-        self.assertEqual(__, len(string))
+        self.assertEqual(len("\n"), len(string))
