656779bac0260334f4a761382f9cf9dfe6d35e8c
mckeane-thomas <mckeane.thomas@gmail.com>
2015-02-12 10:59:15 -0500

    Update about_strings.py

--- python2/koans/about_strings.py
+++ python2/koans/about_strings.py
@@ -37,7 +37,7 @@ class AboutStrings(Koan):
     def test_use_backslash_for_escaping_quotes_in_strings(self):
         a = "He said, \"Don't\""
         b = 'He said, "Don\'t"'
-        self.assertEqual("He said, \"Don't\"", (a == b))
+        self.assertEqual(a, b)
 
     def test_use_backslash_at_the_end_of_a_line_to_continue_onto_the_next_line(self):
         string = "It was the best of times,\n\
